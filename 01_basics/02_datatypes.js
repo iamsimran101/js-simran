735f11cf7d48a107c264701614faadb47ca359e6
iamsimran101 <118089219+iamsimran101@users.noreply.github.com>
2024-06-17 11:23:04 +0000

    datatypes with conversion of it

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+//alert (3+3)// we are using nodejs not browser!so, alert is not executed in node but only in  browser i.e:- popup message 
+
+//console.log(4+4)
+
+//console.log("Simran")
+
+//learning about conversions 
+
+let marks = null
+
+let marksInBoolean = Boolean(marks)
+
+console.log(typeof marksInBoolean );
+
+console.log(  marksInBoolean); 
+
