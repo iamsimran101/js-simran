61ec49b6a5d3a4b86107c67f5c9124464e865ce0
iamsimran101 <118089219+iamsimran101@users.noreply.github.com>
2024-06-28 08:43:01 +0000

    container config done

---
+++ 01_basics/04_numsandmaths.js
@@ -0,0 +1,7 @@
+console.log(Math.min(2,8));
+console.log(Math.max(4,8));
+
+console.log(Math.random());
+
+//basically here in number and maths , we learned about number conversion in short 
+//and in math which in short is a default value in js we learned about random() which will be used in gaming dices game and statistics 
